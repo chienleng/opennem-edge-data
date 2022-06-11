9dbbaaa1ff1c28f629e98ae1ea0fedb505e150f9
Steven <steven@steventan.com.au>
2022-06-11 23:12:23 +0800

    Create quarter.json.js

---
+++ src/routes/data/au/emissions/quarter.json.js
@@ -0,0 +1,18 @@
+import Papa from 'papaparse';
+
+export async function get() {
+	const response = await fetch(
+		'https://raw.githubusercontent.com/opennem/emissions-csv/main/data/nggi-quarterly-update.csv'
+	);
+
+	const csv = await response.text();
+	const parsed = Papa.parse(csv, { header: true });
+
+	return {
+		// @ts-ignore
+		body: parsed.data,
+		headers: {
+			'Access-Control-Allow-Origin': '*'
+		}
+	};
+}
